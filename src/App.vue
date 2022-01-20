ef1699d082d535c01c19509ad0b56508e93fea6f
Shreyas Ananth <shreyas.ananth@shakedeal.com>
2022-01-20 13:26:02 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="logo" src="./assets/clock.jpg">
+    <Time/>
+  </div>
+</template>
+
+<script>
+import Time from './components/showTime.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Time
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
